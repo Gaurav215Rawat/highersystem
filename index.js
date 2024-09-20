5806abb9a8c748b60e3fb991ff5708e15c190df9
Gaurav Rawat <123564858+Gaurav215Rawat@users.noreply.github.com>
2024-09-20 16:31:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -197,8 +197,7 @@ app.post('/verify-token', (req, res) => {
 // Route for user registration (signup)
 app.post('/signup', [
   body('email').isEmail().withMessage('Invalid email address'),
-  body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long'),
-  body('dept_id').isInt().withMessage('Invalid department ID')
+  body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
 ], async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
