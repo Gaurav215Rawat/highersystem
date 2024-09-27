cf9da83051f32af3ec4d356f609af64708bf350c
Gaurav Rawat <123564858+Gaurav215Rawat@users.noreply.github.com>
2024-09-27 12:14:28 +0530

    Update setuptable.js

--- setuptable.js
+++ setuptable.js
@@ -2,34 +2,48 @@ const { Client } = require('pg');
 const config = require('./config');
 
 const createTablesQuery = `
-DROP TABLE IF EXISTS contacts;
-DROP TABLE IF EXISTS customers;
-DROP TABLE IF EXISTS users CASCADE;
+ DROP TABLE IF EXISTS contacts CASCADE;
+    DROP TABLE IF EXISTS customers CASCADE;
+    DROP TABLE IF EXISTS users CASCADE;
+    DROP TABLE IF EXISTS api_access CASCADE;
+    DROP TABLE IF EXISTS departments CASCADE;
 
-   CREATE TABLE users (
-  user_id SERIAL PRIMARY KEY,
+
+     CREATE TABLE IF NOT EXISTS departments (
+      dept_id SERIAL PRIMARY KEY,
+      dept_name VARCHAR(20) UNIQUE NOT NULL,
+      dept_data Text,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+    CREATE TABLE IF NOT EXISTS users (
+      user_id SERIAL PRIMARY KEY,
       first_name VARCHAR(20) NOT NULL,
       last_name VARCHAR(20) NOT NULL,
       email VARCHAR(30) UNIQUE NOT NULL,
       phone_no VARCHAR(15) UNIQUE NOT NULL,
       password TEXT NOT NULL,
+      dept_name VARCHAR(20) REFERENCES departments(dept_name)  ON DELETE CASCADE,
+      location VARCHAR(20) NOT NULL,
+      emp_id VARCHAR(20) NOT NULL,
+      role VARCHAR(20) NOT NULL,
+      user_status VARCHAR(10) CHECK (status IN ('active', 'inactive')),
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-   );
+    );
 
-
-   CREATE TABLE IF NOT EXISTS customers (
+    CREATE TABLE IF NOT EXISTS customers (
       customer_id SERIAL PRIMARY KEY,
       customer_name VARCHAR(100) NOT NULL,
-      gst_number VARCHAR(15) UNIQUE,
-      landline_num VARCHAR(15) UNIQUE,
-      email_id VARCHAR(100) UNIQUE,
+      gst_number VARCHAR(15),
+      landline_num VARCHAR(15) UNIQUE NOT NULL,
+      email_id VARCHAR(100) UNIQUE NOT NULL,
       pan_no VARCHAR(10) UNIQUE,
       tan_number VARCHAR(15) UNIQUE,
       address TEXT,
-      city VARCHAR(50),
-      state VARCHAR(50),
-      country VARCHAR(50),
-      pincode VARCHAR(10),
+      city VARCHAR(50) NOT NULL,
+      state VARCHAR(50) NOT NULL,
+      country VARCHAR(50) NOT NULL,
+      pincode VARCHAR(10) NOT NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
@@ -41,17 +55,23 @@ DROP TABLE IF EXISTS users CASCADE;
       phone_num VARCHAR(15) UNIQUE,
       email_id VARCHAR(100) UNIQUE,
       address TEXT,
-      city VARCHAR(50),
-      state VARCHAR(50),
-      country VARCHAR(50),
-      pincode VARCHAR(10),
-      department VARCHAR(100),
+      city VARCHAR(50) NOT NULL,
+      state VARCHAR(50) NOT NULL,
+      country VARCHAR(50) NOT NULL,
+      pincode VARCHAR(10) NOT NULL,
+      department VARCHAR(100) NOT NULL,
       designation VARCHAR(100),
-      date_of_start VARCHAR(11),
-      date_of_end VARCHAR(11),
+      date_of_start TEXT,
+      date_of_end TEXT,
       status VARCHAR(10) CHECK (status IN ('active', 'inactive')),
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+    CREATE TABLE IF NOT EXISTS api_access (
+      access_id SERIAL PRIMARY KEY,
+      user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+      api_name VARCHAR(100)
     );
 `;
 
