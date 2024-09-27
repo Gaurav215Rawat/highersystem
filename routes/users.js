481493d8719c60321fa1d702b023828532b04909
Gaurav Rawat <123564858+Gaurav215Rawat@users.noreply.github.com>
2024-09-27 22:27:24 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Client } = require('pg');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const config = require('../config');
 
 const client = new Client(config.database);
